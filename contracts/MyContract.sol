d9fb7a7fd2bf1bc702a30601f6697089f97ee7db
immanual19 <immanuelsarker1@gmail.com>
2023-03-10 16:52:51 +0600

    2ndCommit

--- contracts/MyContract.sol
+++ contracts/MyContract.sol
@@ -2,14 +2,17 @@ pragma solidity ^0.8.12;
 
 contract MyContract{
     //State Variable
-    //Local Variable
-
-
+    int256 public myInt = -16055;
+    uint public myUint = 1;
+    uint256 public myUint256 = 1;
     function getValue()public pure returns(uint){
+        //Local Variable
         uint value=1;
         value++;
         return value;
     }
+
+    string public myName="A. Immanual Sarker";
 
     function add(uint x, uint y) public pure returns(uint){
         return x+y;
