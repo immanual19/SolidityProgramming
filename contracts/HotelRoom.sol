d408c321db03cecd430997ae3872249f41ddce04
immanual19 <immanuelsarker1@gmail.com>
2023-03-11 17:38:24 +0600

    HotelBooking

---
+++ contracts/HotelRoom.sol
@@ -0,0 +1,51 @@
+pragma solidity ^0.8.12;
+
+contract HotelRoom{
+    //Ether Payments
+    //Modifiers
+    //Visibility
+    //Events
+    //Enums
+
+    enum Statuses{
+        Vacant,
+        Occupied
+        }
+
+    Statuses public currentStatus;
+
+    event Occupy(address _occupant,uint _value);
+
+    address payable public owner;
+
+    constructor(){
+        owner=payable(msg.sender);
+        currentStatus=Statuses.Vacant;
+    }
+
+    modifier onlyWhileVacant{
+        require(currentStatus==Statuses.Vacant,"Currently Occupied."); //If requires function returns true, rest of the code will be executed
+        _;
+    }
+
+    modifier costs(uint _amount){
+        require(msg.value>=_amount, "Not enough ether provided.");
+        _;
+    }
+
+    function book() public payable onlyWhileVacant costs(2 ether){
+
+        //Check Price
+        //Check Status
+
+      
+        
+        //transfer function sends the cryptocurrency the receiver
+        currentStatus=Statuses.Occupied;
+        owner.transfer(msg.value);
+        (bool sent, bytes memory data)=owner.call{value:msg.value}("");
+        require(true);
+        emit Occupy(msg.sender, msg.value);
+    }
+
+}
